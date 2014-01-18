f55d6fc37b633f04fa4f44224d30f5f229de8ee6
oeihiko <oeihiko@gmail.com>
2014-01-18 12:22:29 -0500

    Added isInstalled(package) for dpkg and rpm.

--- tools/environment.py
+++ tools/environment.py
@@ -3,9 +3,22 @@ import fnmatch
 import os
 import string
 import sys
+import subprocess
 
-allDistros = ['linuxmint','ubuntu','debian','opensuse','arch','pclinuxos',
-    'centos','mageia','slackware','crunchbang']
+allDistros = {
+'linuxmint':'apt',
+'ubuntu':'apt',
+'debian':'apt',
+'opensuse':'zypper',
+'arch':'pacman',
+'pclinuxos':'apt',
+'centos':'yum',
+'mageia':'urpmi',
+'redhat':'yum',
+'fedora':'yum',
+'slackware':'slackpkg',
+'crunchbang':'apt'
+}
 
 def isLinux():
   if platform.system() == 'Linux':
@@ -30,8 +43,48 @@ def getDistros():
         data = f.read()
         data = string.lower(data)
         data = data.replace("\\s+","")
-        for distro in allDistros:
+        for distro in allDistros.keys():
           if data.find(distro) != -1:
             distros.append(distro)
-  return distros
+  return list(set(distros))
+
+def getPMSCandidates():
+  managers = []
+  distros = getDistros()
+  for distro in distros:
+    managers.append(allDistros[distro])
+  return list(set(managers))
+
+def isInstalled(package):
+  for manager in getPMSCandidates():
+    if manager == 'apt':
+      p = subprocess.Popen(
+          ["sudo","dpkg","-s",package],
+          stdin = subprocess.PIPE,
+          stdout = subprocess.PIPE,
+          stderr = subprocess.PIPE
+          )
+      while p:
+        line = p.stdout.readline()
+        if not line:
+          break
+        if line.find("Package: ") != -1:
+          return True
+    elif manager == 'yum' or manager == 'zypper':
+      p = subprocess.Popen(
+          ["sudo","rpm","-qa", "|", "grep", package],
+          stdin = subprocess.PIPE,
+          stdout = subprocess.PIPE,
+          stderr= subprocess.PIPE
+          )
+      while p:
+        line = p.stdout.readline()
+        if not line:
+          break
+        if line.size > 0:
+          return True
+
+  return False
+
+
 
